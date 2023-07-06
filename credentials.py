fa34f9d709b17c6cdbfc2de0869fd9ae12e30708
kostogls <38495639+kostogls@users.noreply.github.com>
2023-07-06 16:40:57 +0300

    Delete credentials.py

--- credentials.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-def define_credentials():
-    # username = os.environ["DB_USER"]
-    # pwd = os.environ["DB_PWD"]
-    username = 'sofiak'
-    pwd = 'QEfTRxwcy5X6UcCa'
-    return username, pwd
