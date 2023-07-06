b83988d426863b84b90ff8001077c9d4f6003fbe
kostogls <38495639+kostogls@users.noreply.github.com>
2023-07-06 16:40:40 +0300

    Update db_connection.py

--- db_connection.py
+++ db_connection.py
@@ -3,8 +3,6 @@ import certifi
 
 # from credentials import define_credentials
 
-username = 'sofiak'
-pwd = 'QEfTRxwcy5X6UcCa'
 
 def connect_db():
     ca = certifi.where()
@@ -15,23 +13,6 @@ def connect_db():
     db = client.testdb
     # print(db)
     return db
-
-
-# def connect_db():
-#     client = MongoClient("mongodb://root:VNpVXTV2@sdg4.csd.auth.gr:28318")
-#     db = client.database
-#     # for coll in db.list_collection_names():
-#     #     print(coll)
-#     # print(db)
-#     try:
-#         db.command("serverStatus")
-#         # print('herew')
-#     except Exception as e:
-#         print(e)
-#     else:
-#         print("You are connected!")
-#     # client.close()
-#     return db
 
 
 connect_db()
